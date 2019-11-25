c5f1bb1fff3f15a3a9b1ee14ac3f9e906dddaa72
kevaltech9teen <kevaltech9teen@gmail.com>
2019-11-25 23:56:00 +0530

    student listing design

--- lib/screens/studentList.dart
+++ lib/screens/studentList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:bss_admin/common/constant.dart' as cnst;
 
 class studentList extends StatefulWidget {
   @override
@@ -19,7 +20,108 @@ class _studentListState extends State<studentList> {
         itemBuilder: (BuildContext context, int index) {
           return Card(
             margin: EdgeInsets.all(5),
-            child: Text("aa"),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  ClipOval(
+                    child: Image.asset(
+                      "images/my_photo.jpeg",
+                      width: 70,
+                      height: 70,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 4)),
+                  Text(
+                    "102",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Text(
+                    "Chirag Mevada",
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                        color: cnst.fontColors),
+                  ),
+                  Text(
+                    "Cricket",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 5)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Opacity(
+                            child: Container(
+                              width: 37,
+                              height: 37,
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(50))),
+                              alignment: Alignment.center,
+                            ),
+                            opacity: 0.15,
+                          ),
+                          Icon(
+                            Icons.call,
+                            color: Colors.green[700],
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Opacity(
+                            child: Container(
+                              width: 37,
+                              height: 37,
+                              decoration: BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(50))),
+                              alignment: Alignment.center,
+                            ),
+                            opacity: 0.15,
+                          ),
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.blueAccent,
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Opacity(
+                            child: Container(
+                              width: 37,
+                              height: 37,
+                              decoration: BoxDecoration(
+                                  color: Colors.orange,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(50))),
+                              alignment: Alignment.center,
+                            ),
+                            opacity: 0.15,
+                          ),
+                          Icon(
+                            Icons.more_horiz,
+                            color: Colors.orange,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           );
         },
         staggeredTileBuilder: (_) => StaggeredTile.fit(2),
